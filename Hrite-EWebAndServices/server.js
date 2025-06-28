52c0f7538d4cff835ea6b89c26b79751bb5d940e
viqSTAR <prasadmanthan07@gmail.com>
2025-06-28 12:41:15 +0530

    updater server

--- Hrite-EWebAndServices/server.js
+++ Hrite-EWebAndServices/server.js
@@ -12,13 +12,13 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// app.get('/about', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'about.html'));
-// });
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
 
-// app.get('/contact', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'contact.html'));
-// });
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
